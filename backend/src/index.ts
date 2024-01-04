7f9413948379f414ffbb78ada7d8387a9ebd0480
Riju Manandhar <rijumd@gmail.com>
2024-01-04 13:54:37 +0545

    opened server and connected to mysql

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql' ;
+import cors from 'cors' //TODO: to connect to REACT apparently
+import { createConnection } from 'typeorm'; //to connect to db
+
+const main = async () => {
+
+    await createConnection({
+        type: "mysql",
+        database: "graphqlcrud",
+        username: "root",
+        password: "@dmin123",
+        logging: true,
+        synchronize: true,
+        entities: [],
+    });
+
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+    // app.use("/graphql", graphqlHTTP({
+    //     schema,
+    //     graphiql: true //api tester provided by graphql
+    // }))
+
+    app.listen(3001, ()=>{
+        console.log("SERVER RUNNING ON PORT 3001")
+    })
+};
+
+main().catch((err) =>{
+    console.log(err);
+});
