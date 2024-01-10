11d4f870d9b947fcdcf195477282bf73fa2ea490
Riju Manandhar <rijumd@gmail.com>
2024-01-10 22:54:33 +0545

    added add-company component

---
+++ frontend/angular-graphql/src/app/company/add-company/add-company.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-company',
+  templateUrl: './add-company.component.html',
+  styleUrl: './add-company.component.css'
+})
+export class AddCompanyComponent implements OnInit {
+  ngOnInit(): void {
+   
+  }
+}
