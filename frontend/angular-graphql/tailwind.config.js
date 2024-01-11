f1128fe24323f6a119fe9bdfd57c7ec53d04e5d2
Riju Manandhar <rijumd@gmail.com>
2024-01-11 20:01:48 +0545

    added theme color and a placeholder image

--- frontend/angular-graphql/tailwind.config.js
+++ frontend/angular-graphql/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-gray" : "#F7F7F7;",
+        "primary-yellow" : "#FFE68C",
+        "accent-orange" : "#f97316",
+        "accent-yellow" : "#fcd34d",
+        "accent-blue" : "#404495",
+        "button-orange" : "#F85A25",
+        "button-gray" : "#EEF1F7",
+      },
+    },
   },
   plugins: [],
 }
